cfa11b440ad8cb00f84df0516744ab28ebd93c20
Igor Wnek <igowne@gmail.com>
2023-10-24 00:06:57 +0200

    init: set up project basing on my Node boilerplate

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+const jestConfig: JestConfigWithTsJest = {
+    preset: 'ts-jest/presets/default-esm',
+    testEnvironment: 'node',
+    extensionsToTreatAsEsm: ['.ts'],
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
+    collectCoverage: true,
+}
+
+export default jestConfig
